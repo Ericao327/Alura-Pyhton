23bed609985da976e438fe51f580f3b1c90e2b7d
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-06-25 16:30:49 -0300

    Construtor e instanciando objetos

--- oo - sabor express/Modelos/restaurante.py
+++ oo - sabor express/Modelos/restaurante.py
@@ -1,14 +1,22 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Italiana'
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_pizza = Restaurante()
+    def __str__(self):
+        return f'{self.nome}' | {self.categoria}
+
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}' )
+
+restaurante_praca = Restaurante('Praça', 'Italiana')
+restaurante_pizza = Restaurante('Pizza', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurante_praca.ativo)
+Restaurante.listar_restaurantes()
