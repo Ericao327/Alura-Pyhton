2907450825acb2a457c50f204f52d2414c114090
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-06-29 10:24:06 -0300

    From and Import

--- oo - sabor express/Modelos/restaurante.py
+++ oo - sabor express/Modelos/restaurante.py
@@ -6,26 +6,19 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
-
+    
     def __str__(self):
-        return f'{self._nome}' | {self.categoria}
-
-    @classmethod 
-
+        return f'{self._nome} | {self._categoria}'
+    
+    @classmethod
     def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}' )
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
-        return 'verdadeiro' if self._ativo else 'false'
-
+        return '⌧' if self._ativo else '☐'
+    
     def alternar_estado(self):
         self._ativo = not self._ativo
-
-restaurante_praca = Restaurante('Praça', 'Italiana')
-restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('Pizza', 'Italiana')
-
-Restaurante.listar_restaurantes()
