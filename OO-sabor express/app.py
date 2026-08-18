a909bd67196f95d72342d323c7ec9e6f8976f715
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-08-17 21:45:20 -0300

    Usando o endopoint

--- OO-sabor express/app.py
+++ OO-sabor express/app.py
@@ -1,4 +1,5 @@
 import requests 
+import json
 
 url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
 
@@ -8,18 +9,20 @@ print(response)
 
 if response.status_code == 200: 
     dados_json = response.json()
-    dados_restaurantes = {}
+    dados_restaurante = {}
     for item in dados_json:
         nome_do_restaurante = item['Company']
-    if nome_do_restaurante not in dados_restaurantes:
-        dados_restaurantes[nome_do_restaurante] = []
-    dados_restaurantes[nome_do_restaurante].append({
+    if nome_do_restaurante not in dados_restaurante:
+        dados_restaurante[nome_do_restaurante] = []
+    dados_restaurante[nome_do_restaurante].append({
         "item": item['Item'],
         "price": item['Price'],
         "descricao": item['Description']
     })
     
+for nome_do_restaurante, dados in dados_restaurante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo, 'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
 else:
     print(f'O erro foi {response.status_code}')
-
-print(dados_restaurantes['McDonalds']) 
